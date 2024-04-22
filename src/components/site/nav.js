a57eb314de7b6a4f339de27d559ab57c76738401
Oskar Rough <oskar@rough.dk>
2024-04-22 13:33:41 +0200

    Lint errors to fix deploy

--- src/components/site/nav.js
+++ src/components/site/nav.js
@@ -1,27 +1,12 @@
-import {NavLink as Link, useNavigate} from 'react-router-dom'
+import {NavLink as Link} from 'react-router-dom'
 import {DbSessionContext} from 'contexts/db-session'
-import CommandMenu from 'components/site/command-menu'
-import createCommands from 'utils/commands'
 
-export default function Nav(props) {
-	const navigate = useNavigate()
-
+export default function Nav() {
 	return (
 		<DbSessionContext.Consumer>
-			{({session, setUserChannel, userChannel, userChannels}) => {
-				const commands = createCommands({isSignedIn: session, navigate, userChannel})
-
-				/* const handleChannelChanged = ({target: {name, value: selectedChannelSlug}}) => {
-					 const selectedChannel = userChannels.find((channel) => {
-					 return channel.slug === selectedChannelSlug
-					 })
-					 setUserChannel(selectedChannel)
-					 navigate(`/${selectedChannelSlug}/`)
-					 } */
-
+			{({session,  userChannels}) => {
 				return (
 					<>
-						{/* <CommandMenu commands={commands}></CommandMenu> */}
 						<menu>
 							<li>
 								<Link to="/">Radio4000 migration (v1 → v2)</Link>
@@ -30,18 +15,6 @@ export default function Nav(props) {
 									<>
 										{/* <Link to="/account">Account</Link> */}
 										{/* <Link to="/channels" end>Channels</Link> */}
-										{userChannels?.length ? (
-											<>
-											{/*}
-												<SelectUserChannel
-													userChannel={userChannel}
-													userChannels={userChannels}
-													onChange={handleChannelChanged}
-												/>
-										*/}
-												{/* <Link to="/create/track">+track</Link> */}
-											</>
-										) : null}
 									</>
 								)}
 							</li>
